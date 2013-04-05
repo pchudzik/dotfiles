b1dbad3f97281adb6b3917d93761d61320a1af6a
Paweł Chudzik <pawel.chudzik@netsprint.eu>
2013-04-05 11:46:18 +0200

    * setup do wlaczania zmian

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+function backupOriginal() {
+	FILE=$1;
+	BACKUP="$FILE.bak"
+	if [ -f "$FILE" ]; then
+		mv "$FILE" "$BACKUP"
+		echo "$FILE backuped in $BACKUP"
+	else
+		echo "No original file $FILE"
+	fi;
+}
+
+function addFile() {
+	ORG_FILE="$1"
+	MY_FILE="$2"
+
+	backupOriginal $ORG_FILE;
+	ln -s "$MY_FILE" "$ORG_FILE"
+	echo "$MY_FILE linked to $ORG_FILE"
+}
+
+DIR=`pwd`
+addFile "$HOME/.vimrc" "$DIR/vimrc"
